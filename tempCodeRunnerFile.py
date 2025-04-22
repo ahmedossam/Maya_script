87c59153d3f89157e0da498205bd2bffe38e4572
ahmedossam <69688332+ahmedossam@users.noreply.github.com>
2025-04-22 15:22:36 +0200

    vectors

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,18 @@
-class Models:
-    def __init__(self, name, vertexes, edges, faces):
-        self.name = name
-        self.vertexes = vertexes
-        self.edges = edges
-        self.faces = faces
-        pass
-    def __str__(self):
-        return f"Name: {self.name}, vertexes: {self.vertexes}, edges: {self.edges}, faces: {self.faces}"
-    pass
-
-Cube = Models("Cube", 8, 12, 6)
-Sphere = Models("Sphere", 140, 40, 50)
-Cylinder = Models("Cylinder", 20, 30, 10)
-Cone = Models("Cone", 10, 20, 5)
-print(Cube)
-print(Sphere)
-
-print(Cylinder)
-print(Cone) 
+import math 
+class vector3D:
+    def __init__(self, x, y, z):
+       createdvector=0
+       self.x = x       
+       self.y = y
+       self.z = z
+       vector3D.createdvector+=1
+    @classmethod
+    def get_created_vector(cls):
+         return cls.createdvector
+    def distance(V1,v2):
+        return math.sqrt((V1.x - v2.x)**2 + (V1.y - v2.y)**2 + (V1.z - v2.z)**2)
+        
+v=vector3D(1,2,3)
+v2=vector3D(4,5,6)
+distance = vector3D.distance(v,v2)
+print(f"Distance between v1 and v2 is: {distance}")
